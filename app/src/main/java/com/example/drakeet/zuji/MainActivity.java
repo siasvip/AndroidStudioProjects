f2b6e63017808e16f33cfb7c829e4f8d0bf17333
Drakeet <siasvip@gmail.com>
2017-12-09 16:15:00 +0800

    第一次同步足迹代码！！！

---
+++ app/src/main/java/com/example/drakeet/zuji/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.drakeet.zuji;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.MapView;
+
+public class MainActivity extends AppCompatActivity {
+    private MapView mMapView = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        SDKInitializer.initialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+        //获取地图控件引用
+        mMapView = (MapView) findViewById(R.id.bmapView);
+        // 不显示百度地图Logo
+        mMapView.removeViewAt(1);
+
+
+    }
+
+
+    @Override
+        protected void onDestroy() {
+            super.onDestroy();
+            //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+            mMapView.onDestroy();
+        }
+        @Override
+        protected void onResume() {
+            super.onResume();
+            //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+            mMapView.onResume();
+        }
+        @Override
+        protected void onPause() {
+            super.onPause();
+            //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+            mMapView.onPause();
+        }
+    }
+
